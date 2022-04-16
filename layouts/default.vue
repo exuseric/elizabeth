7afdf79efc5d4f2c600b2f2487f41cbfd6edbb14
Eric Maina <ericbro29@gmail.com>
2022-04-16 15:28:52 +0300

    Initial Setup

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="home">
+    <header>
+      <h1>Elizabeth Nthena</h1>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  layout: 'default',
+}
+</script>
+
+<style lang="scss" scoped></style>
